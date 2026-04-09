0531b74300958464cfe580cf9c1de56916c1b73e
AndreyHalitskyi <galickiy2006@gmail.com>
2026-04-09 16:49:21 +0300

    init: backend setup + swagger + health check

---
+++ SmashHub.Api/Controller/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmashHub.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("API is working");
+        }
+    }
+}
