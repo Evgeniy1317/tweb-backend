74e1add70846406fd94acc61d776ce8ad56961cf
AndreyHalitskyi <galickiy2006@gmail.com>
2026-04-20 00:20:09 +0300

    add new Domain entities

---
+++ SmashHub.Domain/Court.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmashHub.Domain
+{
+    public class Court
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Hours { get; set; } = string.Empty;
+        public string Coach { get; set; } = string.Empty;
+        public string CoachPhone { get; set; } = string.Empty;
+        public int Courts { get; set; }
+        public string Image { get; set; } = string.Empty;
+    }
+}
