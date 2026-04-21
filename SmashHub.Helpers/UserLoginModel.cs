fd6bd9fe1023fbcc66df27827282e2370208cd83
AndreyHalitskyi <galickiy2006@gmail.com>
2026-04-21 12:51:02 +0300

    Refactor DTOs and move them to Helpers

---
+++ SmashHub.Helpers/UserLoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SmashHub.Helpers
+{
+    public class UserLoginModel
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
