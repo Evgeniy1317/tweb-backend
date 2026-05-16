745967f6ffb0acaecf93ceef951706013d80df1c
AndreyHalitskyi <galickiy2006@gmail.com>
2026-05-16 14:42:50 +0300

    chore(cors): allow second frontend dev port

--- SmashHub.Api/Program.cs
+++ SmashHub.Api/Program.cs
@@ -34,7 +34,9 @@ builder.Services.AddSwaggerGen(options =>
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(
+                  "http://localhost:5173",
+                  "http://localhost:5174")
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
